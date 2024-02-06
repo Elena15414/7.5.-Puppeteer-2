3e039b1a8c6b912dfd6d706cabdc01ec521e3517
Elena Datsenko <elenasheglova15414@gmail.com>
2024-02-06 00:23:06 -0800

    read task1

--- net.test.js
+++ net.test.js
@@ -4,7 +4,7 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
+  await page.setDefaultNavigationTimeout(70000);
 });
 
 afterEach(() => {
@@ -28,7 +28,7 @@ describe("let's go to the cinema tests", () => {
     test("The first link text 'Красивый зал'", async () => {
     const actual = await getText(page, ".movie-seances__hall-title");
     expect(actual).toContain("Красивый зал");
-  }), 70000;
+  }, 70000);
 
   test("Treservation for 18 00", async () => {
    const btnSelector = ".movie-seances__time";
@@ -37,6 +37,37 @@ describe("let's go to the cinema tests", () => {
     });
     const actual = await getText(page, ".movie-seances__time");
     expect(actual).toContain("11:00")
-  }), 70000;
+  },70000);
 
+});
+
+describe("reservation cinema tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://qamid.tmweb.ru/client/hall.php");
   });
+ 
+   test ("Mickey Mouse positive", async() =>{ 
+    await clickElement(page, ".page-nav__day:last-child");
+    await clickElement(page, "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a"); 
+    await clickElement(page, ".buying-scheme__chair_standart");
+    await page.click(".acceptin-button", {
+      visible: true,
+    });
+    const actual = await getText(page, ".acceptin-button");
+    expect(actual).toContain("Забронировать");
+  }, 70000);
+
+    test ("Mickey Mouse negative", async() =>{
+    await clickElement(page, ".page-nav__day:last-child");
+    await clickElement(page, "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a"); 
+    await clickElement(page, ".buying-scheme__chair_standart");
+    await page.click(".acceptin-button");
+    await page.click(".acceptin-button", {
+      visible: false,
+    });
+    const actual = await getText(page, ".acceptin-button");
+    expect(actual).toContain("Забронировать");
+  }, 70000);
+
+});
