cc9e30103c53cd3f2f04b4b87b987807d960a981
Elena Datsenko <elenasheglova15414@gmail.com>
2024-02-08 00:18:09 -0800

    cucumber

--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,11 +1,15 @@
+const fetch = require("node-fetch");
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
+const { putText, getText, clickElement } = require("../../lib/commands.js");
+var {setDefaultTimeout} = require("cucumber");
+setDefaultTimeout(60 * 1000);
+
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const browser = await puppeteer.launch({ headless: false});
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
@@ -17,18 +21,58 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://qamid.tmweb.ru/client/index.php${string}`, {
-    setTimeout: 20000,
+Given("user is on page1 {string}", async function (string) {
+  return await this.page.goto(`https://qamid.tmweb.ru/client/index.php`, {
+    setTimeout: 50000,
   });
 });
+When("user selects the desired day", async function () {
+  return await clickElement(this.page, ".page-nav__day:last-child");
+}, 70000);
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+Then ("user selects the desired movie", async function () {
+  return await clickElement (
+    this.page,"body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(1) > a");
+}, 70000);
+
+Then ("user chooses a location", async function () {
+  return await clickElement(this.page, ".buying-scheme__chair_standart");
+}, 70000);
+
+Then ("user is booking tickets", async function () {
+  return await clickElement(this.page, ".acceptin-button");
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+Then("user confirms the booking {string}", async function (string) {
+  const actual = await getText(this.page, ".acceptin-button");
+  expect(actual).toContain("Забронировать");
+}, 70000);
+
+
+Given("user is on page2 {string}", async function (string) {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
+    setTimeout: 30000,
+  });
+});
+When("user selects the desired day2", async function () {
+  return await clickElement(this.page, ".page-nav__day:last-child");
+});
+Then ("user selects the desired movie2", async function () {
+  return await clickElement(
+    this.page,
+    "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a"
+  );
+});
+Then ("user selects the occupied place", async function () {
+  return await clickElement(this.page, ".buying-scheme__chair_standart");
+});
+Then ("user has booked tickets", async function () {
+  return await clickElement(this.page, ".acceptin-button");
+});
+Then ("user has confirmed the booking of tickets", async function () {
+  return await clickElement(this.page, ".acceptin-button");
+});
+Then("Get Error {string}", async function (string) {
+  const actual = await getText(this.page, ".acceptin-button");
+  await expect(actual).contain("Забронировать");
 });
--- net.test.js
+++ net.test.js
@@ -5,6 +5,7 @@ let page;
 beforeEach(async () => {
   page = await browser.newPage();
   await page.setDefaultNavigationTimeout(70000);
+  await page.goto("https://qamid.tmweb.ru/client/index.php");
 });
 
 afterEach(() => {
@@ -12,40 +13,31 @@ afterEach(() => {
 });
 
 describe("let's go to the cinema tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://qamid.tmweb.ru/client/index.php");
-  });
 
    test("The first test'", async () => {
     const title = await page.title();
-    //await clickElement("page-header");
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('ИдёмВКино');
   }, 70000);
 
-    test("The first link text 'Красивый зал'", async () => {
+    test("The first link text Зал 1", async () => {
     const actual = await getText(page, ".movie-seances__hall-title");
-    expect(actual).toContain("Красивый зал");
+    expect(actual).toContain("Зал 1");
   }, 70000);
 
-  test("Treservation for 18 00", async () => {
+  test("Treservation for 00:00", async () => {
    const btnSelector = ".movie-seances__time";
    await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await getText(page, ".movie-seances__time");
-    expect(actual).toContain("11:00")
+    expect(actual).toContain("00:00")
   },70000);
 
 });
 
 describe("reservation cinema tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://qamid.tmweb.ru/client/hall.php");
-  });
  
    test ("Mickey Mouse positive", async() =>{ 
     await clickElement(page, ".page-nav__day:last-child");
@@ -60,14 +52,13 @@ describe("reservation cinema tests", () => {
 
     test ("Mickey Mouse negative", async() =>{
     await clickElement(page, ".page-nav__day:last-child");
-    await clickElement(page, "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a"); 
-    await clickElement(page, ".buying-scheme__chair_standart");
+    await clickElement(page, "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a");
+    await clickElement(page, ".buying-scheme__chair_selected");
     await page.click(".acceptin-button");
     await page.click(".acceptin-button", {
       visible: false,
     });
     const actual = await getText(page, ".acceptin-button");
     expect(actual).toContain("Забронировать");
-  }, 70000);
-
+  }, 75000);
 });
